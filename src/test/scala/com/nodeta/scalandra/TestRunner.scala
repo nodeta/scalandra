288bc75384fe8843355392693f7eff288d0c7b8c
Ville Lautanala <ville.lautanala@nodeta.fi>
2009-10-07 11:28:55 +0300

    Updated specs to 1.6.0

--- src/test/scala/com/nodeta/scalandra/TestRunner.scala
+++ src/test/scala/com/nodeta/scalandra/TestRunner.scala
@@ -2,5 +2,5 @@ package com.nodeta.scalandra.tests
 
 import org.specs.runner.SpecsFileRunner
 
-object TestRunner extends SpecsFileRunner("src/test/scala/**/*.scala", ".*",
+object TestRunner extends SpecsFileRunner("src/test/scala/**/*Test.scala", ".*",
   System.getProperty("system", ".*"), System.getProperty("example", ".*"))
