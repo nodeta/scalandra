15a10dd6c01740783d5df74b0d031596254ea73e
Ville Lautanala <ville.lautanala@nodeta.fi>
2010-04-11 21:48:12 +0300

    Download Scala Specs as managed dependency

--- project/build/Scalandra.scala
+++ project/build/Scalandra.scala
@@ -4,6 +4,7 @@ class ScalandraProject(info: ProjectInfo) extends DefaultProject(info) {
   val ibiblioRepo = "iBiblio Maven 2 Repository" at "http://www.ibiblio.org/maven2"
   val commonsPool = "commons-pool" % "commons-pool" % "1.5.4"
   val slf4j = "org.slf4j" % "slf4j-simple" % "1.5.11"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.4" % "test"
 
   override def defaultExcludes = super.defaultExcludes || "map"
 }
