530815c7fb60c271e463f05df92b9328eec8358b
Ville Lautanala <ville.lautanala@nodeta.fi>
2010-04-15 20:44:27 +0300

    Update to Scala 2.8.0.RC1

--- project/build/Scalandra.scala
+++ project/build/Scalandra.scala
@@ -2,9 +2,10 @@ import sbt._
 
 class ScalandraProject(info: ProjectInfo) extends DefaultProject(info) {
   val ibiblioRepo = "iBiblio Maven 2 Repository" at "http://www.ibiblio.org/maven2"
+  val snapshotRepo = "Scala Tools Snapshosts" at "http://scala-tools.org/repo-snapshots/"
   val commonsPool = "commons-pool" % "commons-pool" % "1.5.4"
   val slf4j = "org.slf4j" % "slf4j-simple" % "1.5.11"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.4" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test"
 
   override def defaultExcludes = super.defaultExcludes || "map"
 }
